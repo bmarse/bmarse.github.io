539b7b4ab13ea3d3ef94d23194f56673add8d677
Brandon Martinez <bmartheswe@gmail.com>
2026-06-16 20:46:01 -0500

    Adding heart of gold base site

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,6 +3,7 @@ const { eleventyImageTransformPlugin } = require("@11ty/eleventy-img");
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({
 			"./assets/": "/assets/",
+      "./hog/assets/": "/hog/assets/",
       "./CNAME": "/CNAME",
 		})
 };
